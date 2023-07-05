49d34c31a9552d473397ac258a6b8c5ac425d4d0
Sixto Ortega <ortegasixto7@gmail.com>
2023-07-04 22:24:10 -0400

    Adding application files

---
+++ bank-api/src/main/java/com/ortegasixto7/bankapi/BankApiApplication.java
@@ -0,0 +1,13 @@
+package com.ortegasixto7.bankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApiApplication.class, args);
+	}
+
+}
